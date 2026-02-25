69bc066ca3dc89c3613cbb81f643f562356e53ec
Gavin Aguiar <gaaguiar@microsoft.com>
2026-02-25 16:04:02 -0600

    Add CosmosDBOutputBinding template for csharp

---
+++ CosmosDBOutputBinding.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApp
+{
+    public class CosmosDBFunction
+    {
+        private readonly ILogger<CosmosDBFunction> _logger;
+
+        public CosmosDBFunction(ILogger<CosmosDBFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(CosmosDBFunction))]
+        [CosmosDBOutput("%CosmosDb%", "%CosmosContainerOut%", Connection = "CosmosDbConnection", CreateIfNotExists = true)]
+        public object? Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req,
+            FunctionContext context)
+        {
+            var requestBody = new StreamReader(req.Body).ReadToEndAsync().Result;
+            if (!string.IsNullOrEmpty(requestBody))
+            {
+                _logger.LogInformation("Writing document to Cosmos DB");
+                return new { id = Guid.NewGuid().ToString(), text = requestBody };
+            }
+
+            return null;
+        }
+    }
+}
